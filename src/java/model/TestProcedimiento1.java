1dc2b580425f1b79c64531a4d5d79c5f4fe76ded
JuanTorresSan <francisco-dk@hotmail.com>
2017-04-22 09:39:11 -0500

    Add files via upload

--- src/java/model/TestProcedimiento1.java
+++ src/java/model/TestProcedimiento1.java
@@ -8,13 +8,13 @@ public class TestProcedimiento1 {
     public static void main(String args[])throws Exception{
         Conexion c=new Conexion();
         Connection con=c.conectarse();
-     CallableStatement callate=con.prepareCall("{call GUARDAR_PELICULA(?,?,?)}");
-        callate.registerOutParameter(1,java.sql.Types.INTEGER);
-        callate.setString(2,"EL ARO");
-        callate.setString(3,"Niña del aro, pelocula de terror...");
+     CallableStatement datos=con.prepareCall("{call GUARDAR_PELICULA(?,?,?)}");
+        datos.registerOutParameter(1,java.sql.Types.INTEGER);
+        datos.setString(2,"IT");
+        datos.setString(3,"El payaso terrorifico ");
       
-        callate.execute();
-        int pk=callate.getInt(1);
+        datos.execute();
+        int pk=datos.getInt(1);
         System.out.println("El id ingresado es:"+pk);
     }
     
